e056917d95fa81b1e7e3465d0ffadaca77ad0922
Joe Gesualdo <joegesualdo@gmail.com>
2013-07-22 11:41:51 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeepcodeTasks::Application.config.secret_token = '4e0a19b0cda783cb7030bdcc59a322a177a71c18280bf9f1cbf01e32242b53ea1c88fd16454591e8e46fe9df2fa1cc9e49b1a1bd2d4704917e3dd2a2226e9769'
